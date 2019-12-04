069c3751687cbd64d885c5e5575bcf0bf6ec4120
aaron <aaron@example.com>
2019-12-04 14:51:58 -0800

    Added some files

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
+from .forms import PostForm
 from .models import Post
 
 # Create your views here.
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	Post.objects.get(pk=pk)
 	return render(request, 'blog/post_list.html',{'posts':posts})
-
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+def post_new(request):
+	form = PostForm()
+	return render(request, 'blog/post_deit.html', {'form': form})
